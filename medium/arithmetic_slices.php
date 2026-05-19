d30f965d64fae337c48a09aaabdc1864c10547d0
KOKOUPavel <146478790+KOKOUPavel@users.noreply.github.com>
2026-05-19 16:15:47 +0100

    Implement function to count arithmetic slices

--- medium/arithmetic_slices.php
+++ medium/arithmetic_slices.php
@@ -1,1 +1,28 @@
 
+<?php
+class Solution {
+
+    /**
+     * @param Integer[]
+     * @return Integer
+     */
+    function numberOfArithmeticSlices($nums) {
+        $n = count($nums);
+        if ($n < 3) return 0;
+
+        $count = 0;
+        $curr = 0;
+
+        for ($i = 2; $i < $n; $i++) {
+            if ($nums[$i] - $nums[$i - 1] == $nums[$i - 1] - $nums[$i - 2]) {
+                $curr += 1;
+                $count += $curr;
+            } else {
+                $curr = 0;
+            }
+        }
+
+        return $count;
+    }
+}
+?>
